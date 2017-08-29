9fa500de5da720e902d5b66cf24644b6e0186189
anson <770133694@qq.com>
2017-08-29 22:13:46 +0800

    bulid project

---
+++ src/main/java/com/tencent/latke/demo/HelloProcessor.java
@@ -0,0 +1,10 @@
+package com.tencent.latke.demo;
+
+import org.b3log.latke.servlet.annotation.RequestProcessor;
+
+/**
+ * Created by ansonwen on 2017/8/29.
+ */
+@RequestProcessor
+public class HelloProcessor {
+}
